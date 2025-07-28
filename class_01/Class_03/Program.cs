bf83a2297709a67e576eb5832379dc8703ffb449
manahilabid <soniaaid806@gmail.com>
2025-07-28 01:12:35 -0700

    intro to c#

---
+++ class_01/Class_03/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("this is my first c# file");
+Console.WriteLine("Enter Your name");
+Console.ReadLine();
+Console.WriteLine("Enter Your Email");
+Console.ReadLine();
+Console.ReadKey();
