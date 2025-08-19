c4a998a8e2cdbfc7322c007064a8647316107259
manahilabid <soniaaid806@gmail.com>
2025-08-18 21:35:53 -0700

    public protected and private

---
+++ class_09/class_09/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_09
+{
+    internal class Animal
+    {
+        public string name;
+        public string color;
+        public string sound;
+
+        private void Data()
+        {
+            Console.WriteLine("this is the parent of the private method");
+        }
+    }
+    class Dog : Animal
+    {
+        public void derivedfun()
+        {
+            Console.WriteLine("this is the child class");
+        }
+        public void AnimalData()
+        {
+            Console.WriteLine($"\nName:{name} Color:{color}Sound:{sound}");
+        }
+    }
+    class cat : Animal
+    {
+        public void AnimalData()
+        {
+            Console.WriteLine($"Name:{name} Color:{color} Sound:{sound}");
+        }
+        public void catfun()
+        {
+            this.AnimalData();
+        }
+    }
+}
---
+++ class_09/class_09/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog obj = new Dog();
+            obj.derivedfun();
+
+            obj.name = "Jeni";
+            obj.color = "Brown";
+            obj.sound = "Bark";
+            obj.AnimalData();
+            cat cat = new cat();
+            cat.name = "Kitty";
+            cat.color = "Blue";
+            cat.sound = "Meow";
+            cat.AnimalData();
+            cat.catfun();
+            Console.ReadKey();
+        }
+    }
+}
