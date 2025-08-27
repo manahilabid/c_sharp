e03b27c228766f19da17a87a9c2aedfec7ef13cf
manahilabid <soniaaid806@gmail.com>
2025-08-27 08:07:41 -0700

    Polymorphism

---
+++ class_11/class_11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Wild_Animal wa1 = new Wild_Animal();
+            wa1.Sound();
+
+            Wild_Animal wa2 = new Lion();
+            wa2.Sound();
+
+            Wild_Animal wa3 = new Elephant();
+            wa3.Sound();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ class_11/class_11/Wild Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_11
+{
+    internal class Wild_Animal
+    {
+        public virtual void Sound()
+        {
+            Console.WriteLine("------------The Sound of the Wild Animals------------");
+        } 
+        
+    }
+    class Lion : Wild_Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("The Lion hiss and snarl");
+        }
+    }
+
+    class Elephant : Wild_Animal
+    {
+        public override void Sound() 
+        {
+            Console.WriteLine("The Elephant squeaks, and snorts");
+        }
+    }
+}
