cbfefeca514f49c30ee2091791117f34f9c88630
manahilabid <soniaaid806@gmail.com>
2025-08-28 06:43:41 -0700

    Account Details

---
+++ Account Details/Account Details/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_Details
+{
+    internal class Account
+    {
+        public string AccountHolder;
+        public double Balance;
+
+        public Account(string name , double balance)
+        {
+            this.AccountHolder = name;
+            this.Balance = balance;
+        }
+        public virtual void widthdraw(double amount)
+        {
+            if (amount<=Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Accountholder: {AccountHolder} WidthDraw: {amount} Remaining Balance: {Balance} ");
+
+            }
+            else
+            {
+                Console.WriteLine("Not enough Balance");
+            }
+        }
+    }
+    class SavingAccount : Account
+    {
+        public SavingAccount(string name, double balance) : base(name, balance) { }
+    }
+}
---
+++ Account Details/Account Details/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_Details
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------Account Details---------------");
+            Account acc1 = new Account("manahil" , 3000);
+            acc1.widthdraw(1000);
+            Console.ReadKey();
+        }
+    }
+}
