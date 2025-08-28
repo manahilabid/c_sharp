360178ae1a7c9af24cb92a686e3c3864e459ea19
manahilabid <soniaaid806@gmail.com>
2025-08-28 06:40:32 -0700

    ABSTRACT & INTERFACE

---
+++ class_12/class_12/PC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_12
+{
+    abstract class PC
+    {
+        public abstract void name();
+        public abstract void color();
+        public void detail()
+        {
+            Console.WriteLine("Abstract class and method");
+            Console.WriteLine("This is the Abstract class");
+        }
+    }
+    class Dell : PC
+    {
+        public override void name()
+        {
+            Console.WriteLine("This Pc name is dell");
+        }
+        public override void color()
+        {
+            Console.WriteLine("The color is black");
+        }
+    }
+    class HP : PC
+    {
+        public override void name()
+        {
+            Console.WriteLine("This PC name is HP");
+        }
+        public override void color()
+        {
+            Console.WriteLine("The color is white");
+
+        }
+    }
+}
---
+++ class_12/class_12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_12
+{
+    internal class Program
+    {
+        interface Student
+        {
+            void StudentName();
+            void Subject();
+        }
+        class std1 : Student
+        {
+            public void StudentName()
+            {
+                Console.WriteLine("name:manahil \n class:matric");
+            }
+            public void Subject()
+            {
+                Console.WriteLine("science");
+            }
+        }
+        static void Main(string[] args)
+        {
+          Dell dell = new Dell();
+            dell.name();
+            dell.color();
+
+            PC hP = new HP();
+            hP.name();
+            hP.color();
+
+            Student std1 = new std1();
+            std1.StudentName();
+            std1.Subject();
+
+            Console.ReadKey();
+
+        }
+    }
+}
