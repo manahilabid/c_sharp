0c53755d330973631d5174dccb0b80839ce8ffbc
manahilabid <soniaaid806@gmail.com>
2025-09-02 06:50:06 -0700

    win form

---
+++ Class_14/Class_14/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Class_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.secert = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter userName";
+            // 
+            // UserName
+            // 
+            this.UserName.AutoSize = true;
+            this.UserName.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.UserName.Location = new System.Drawing.Point(45, 94);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(0, 20);
+            this.UserName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(45, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(58, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(346, 49);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Username";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(34, 88);
+            this.name.Multiline = true;
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(372, 68);
+            this.name.TabIndex = 5;
+            this.name.Text = "manahil";
+            // 
+            // secert
+            // 
+            this.secert.Location = new System.Drawing.Point(45, 255);
+            this.secert.Multiline = true;
+            this.secert.Name = "secert";
+            this.secert.Size = new System.Drawing.Size(364, 65);
+            this.secert.TabIndex = 6;
+            this.secert.Text = "123";
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.Color.PeachPuff;
+            this.login.Font = new System.Drawing.Font("Mistral", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.Location = new System.Drawing.Point(69, 348);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(350, 71);
+            this.login.TabIndex = 7;
+            this.login.Text = "Login button";
+            this.login.UseVisualStyleBackColor = false;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.secert);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UserName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label UserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox secert;
+        private System.Windows.Forms.Button login;
+    }
+}
+
---
+++ Class_14/Class_14/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string user = name.Text;
+            string pass = secert.Text;
+            if (user == "manahil" && pass == "123")
+            {
+                MessageBox.Show("welcome"+user,"Success");
+            }
+            else
+            {
+                MessageBox.Show("invalid"+user,"Error");
+            }
+        }
+    }
+}
